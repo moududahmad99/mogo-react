4410daa7399dcd76e8021aae37093f7388a9053b
Moudud Ahmad <sabbirahmedmoudud@gmail.com>
2023-06-20 00:18:04 +0600

    Navbar Area Added

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,9 +1,60 @@
 import React from 'react'
+import './Style.css'
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <React.Fragment>
+            <nav className="navbar navbar-expand-lg">
+                <div className="container">
+                    <a className="navbar-brand" href="##">
+                        <picture>
+                            <img src="assets/logo.png" alt="logo" />
+                        </picture>
+                    </a>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link" href="#about">
+                                    About
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#service">
+                                    Service
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#portfolio">
+                                    Work
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#team">
+                                    Team
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#footer">
+                                    Contact
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </React.Fragment>
+    )
 }
 
 export default Navbar
