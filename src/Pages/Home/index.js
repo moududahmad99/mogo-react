b6e745f884c20bd2e6e5443a6aefd8a26610ea30
Moudud Ahmad <sabbirahmedmoudud@gmail.com>
2023-06-19 23:32:27 +0600

    Setup Environment

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../../Components/Navbar/Navbar'
+
+const Home = () => {
+    return (
+        <React.Fragment>
+            <Navbar />
+        </React.Fragment>
+    )
+}
+
+export default Home
